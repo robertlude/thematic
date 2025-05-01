9de6ea750dcb5d0657ae757c22a91cc53e70e8ff
Robert Lude <robert.lude@gmail.com>
2025-05-01 17:45:54 -0400

    add panel

---
+++ Panel.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+/// External Dependencies
+
+import { ReactNode, RefObject } from 'react'
+
+/// Internal Dependencies
+
+// Contexts
+
+import { useTheme } from './ThemeProvider'
+
+/// Component
+
+export default function Panel({
+  children,
+  className = '',
+  ref,
+}: {
+  children:   ReactNode
+  className?: string
+  ref?:       RefObject<HTMLDivElement | null>
+}) {
+  const { theme } = useTheme()
+
+  return <div
+    className={`${theme.panel} ${className}`}
+    ref      ={ref}
+  >
+    {children}
+  </div>
+}
